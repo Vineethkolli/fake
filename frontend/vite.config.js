765b1f307380bdb740e26d36c34ef6b2651adf1f
Vineethkolli <kz8413@srmist.edu.in>
2024-11-26 14:34:54 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,12 +28,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'logo.svg.png',
+            src: 'logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'logo.svg.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
